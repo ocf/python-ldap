9dab70d6988aa23711e3e0ea84735bb658e915ec
stroeder <stroeder>
2014-09-27 10:18:04 +0000

    Updated copyright and release

--- Doc/conf.py
+++ Doc/conf.py
@@ -36,7 +36,7 @@ master_doc = 'index'
 
 # General substitutions.
 project = 'python-ldap'
-copyright = '2008-2011, python-ldap project team'
+copyright = '2008-2014, python-ldap project team'
 
 # The default replacements for |version| and |release|, also used in various
 # other places throughout the built documents.
@@ -44,7 +44,7 @@ copyright = '2008-2011, python-ldap project team'
 # The short X.Y version.
 version = '2.4'
 # The full version, including alpha/beta/rc tags.
-release = '2.4.10.0'
+release = '2.4.17.0'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
